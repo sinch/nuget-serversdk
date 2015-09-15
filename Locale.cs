d33037cd6565b13268ad324ef58c13cc51dc3fd9
Björn <bjorn.fransson@rebtel.com>
2015-09-15 11:22:48 +0200

    Moved Locale class

--- Locale.cs
+++ Locale.cs
@@ -1,9 +1,8 @@
-﻿namespace Sinch.ServerSdk.Calling
+﻿namespace Sinch.ServerSdk
 {
     public class Locale
     {
         public static readonly Locale EnUs = new Locale("en-US");
-        public static readonly Locale SvSe = new Locale("sv-SE");
 
         public Locale(string code)
         {

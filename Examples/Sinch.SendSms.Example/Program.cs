a1e52a15148daef7b791f393d582239943e89f72
Patrick Purcell <patrick@sinch.com>
2015-09-23 09:40:11 +0200

    Update example for simplicity

--- Examples/Sinch.SendSms.Example/Program.cs
+++ Examples/Sinch.SendSms.Example/Program.cs
@@ -14,8 +14,7 @@ namespace Sinch.SendSms.Example
         private static async Task MainAsync()
         {
             var smsApi = SinchFactory.CreateApiFactory("00000000-0000-0000-0000-000000000000", "AAAAAAAAAAAAAAAAAAAAAA==").CreateSmsApi();
-
-            var sendSmsResponse = await smsApi.Sms("+61491570156", "Hello world.  Sinch SMS here.").WithCli("Custom CLI").Send().ConfigureAwait(false);
+            var sendSmsResponse = await smsApi.Sms("+61491570156", "Hello world.  Sinch SMS here.").Send().ConfigureAwait(false);
 
             await Task.Delay(TimeSpan.FromSeconds(10)); // May take a second or two to be delivered.
 

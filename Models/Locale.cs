93322fd6ffa971c1b0b3bc1f67e85f51fc83f674
Björn <bjorn.fransson@rebtel.com>
2015-10-07 17:37:59 +0200

    New Locales

--- Models/Locale.cs
+++ Models/Locale.cs
@@ -3,6 +3,9 @@
     public class Locale 
     {
         public static readonly Locale EnUs = new Locale { Code = "en-US"};
+        public static readonly Locale FrFr = new Locale { Code = "fr-FR" };
+        public static readonly Locale PlPl = new Locale { Code = "pl-PL" };
+        public static readonly Locale EsEs = new Locale { Code = "es-ES" };
 
         public string Code { get; private set; }
     }

31f1e0837c45d8b1b1daaeb5e372250c0ac68bca
Zoran Chaushev <zoran.chaushev@sinch.com>
2020-10-21 21:14:58 +0200

    support for new sms platform  - messageId type changed from int to string

---
+++ src/Sinch.ServerSdk/Messaging/V2/ISmsApiEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Sinch.ServerSdk.Messaging.Models;
+using Sinch.WebApiClient;
+using SendSmsResponse = Sinch.ServerSdk.Messaging.V2.Models.SendSmsResponse;
+
+namespace Sinch.ServerSdk.Messaging.V2
+{
+    /// <summary>
+    /// Interface defining the Sinch SMS REST API
+    /// </summary>
+    public interface ISmsApiEndpoints
+    {
+        [HttpPost("messaging/v1/sms/{+toNumber}")]
+        Task<SendSmsResponse> SendSms([ToUri] string toNumber, [ToBody] SendSmsRequest request);
+
+        [HttpGet("messaging/v1/sms/{messageId}")]
+        Task<GetStatusResponse> GetSmsStatus([ToUri] string messageId);
+    }
+}
